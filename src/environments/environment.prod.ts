93592c5b28d76202e7e879c0db95b4a757f7e417
HanaHernandez <55150439+HanaHernandez@users.noreply.github.com>
2019-12-12 03:16:54 -0700

    Prueba 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyD_ahmb0OvCeEQBNUxEvvsR7P0xOxym-pM",
+    authDomain: "proyectoinfra-51eca.firebaseapp.com",
+    databaseURL: "https://proyectoinfra-51eca.firebaseio.com",
+    projectId: "proyectoinfra-51eca",
+    storageBucket: "proyectoinfra-51eca.appspot.com",
+    messagingSenderId: "286822224857",
+    appId: "1:286822224857:web:e9e9dbc6088576969d3fc8",
+    measurementId: "G-0F2LGYDSGV"
+  }
 };
